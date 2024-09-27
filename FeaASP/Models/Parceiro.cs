b90f308e1130bf2db0f6eac358bc19cfb0e2cfc7
021.900244 <021.900244@C305-237-026>
2024-09-27 18:06:18 -0300

    Adicionar arquivos de projeto.

---
+++ FeaASP/Models/Parceiro.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FEAasp.Models
+{
+    public class Parceiros
+    {
+        [Key]
+        public int idParceiro { get; set; }
+        public string NomeEmpresa { get; set; }
+        public int CNPJ { get; set; }
+        public int telefoneEmpresa { get; set; }
+        public string CEP { get; set; }
+        public string EmailEmpresa { get; set; }
+        public string msgparceiro { get; set; }
+    }
+}
